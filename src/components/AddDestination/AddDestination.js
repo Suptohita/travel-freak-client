74e199a8590ef7abad12f6c25b593fe4252f7c1f
Arvind <arbindudebnath59985@gmail.com>
2021-10-31 23:48:33 +0600

    checking

--- src/components/AddDestination/AddDestination.js
+++ src/components/AddDestination/AddDestination.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Footer from '../Home/Footer/Footer';
+import Navbar from '../Home/Navbar/Navbar';
 
 const AddDestination = () => {
+
+
     return (
         <div>
-            add destination
+            <Navbar></Navbar>
+            <div className='container mt-5 pt-5 pb-5 mb-5' style={{maxWidth:'50%'}}>
+                <h5 className='mt-3'>Name:</h5>
+                <input className='form-control' type="text" name="Name" id="" />
+
+                <h5 className='mt-3'>Price:</h5>
+                <input className='form-control' type="text" name="Email" id="" />
+
+                <h5 className='mt-3'>Image URL:</h5>
+                <input className='form-control' type="text" name="Image_URL" id="" />
+
+                <h5 className='mt-3'>Description:</h5>
+                <input className='form-control' type="text" name="Description" id="" />
+
+                <button className='btn btn-secondary mt-4'>Submit</button>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
