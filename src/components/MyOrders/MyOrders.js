4f4dda07c07fe3f79822b3e64e06b898e6187334
Arvind <arbindudebnath59985@gmail.com>
2021-10-31 19:56:20 +0600

    users order page done

--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -15,37 +15,55 @@ const MyOrders = () => {
     }, [])
 
 
+    const handleCancleOrder = (id) => {
+        const proceed = window.confirm("Are you sure!! You want to delete this order...")
+        if (proceed) {
+
+            // fetch for delete user order 
+            fetch(`http://localhost:8000/myorders/deleteorder/${id}`, {
+                method: "DELETE",
+                headers: { "Content-type": "application/json" }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('Deleted Successfully')
+                        const remainOrder = userOrders.filter(userOrder => userOrder._id !== id)
+                        setUserOrders(remainOrder)
+                    }
+                })
+        }
+    }
+
+
     return (
         <div>
             <Navbar></Navbar>
             <div>
-            <h3 className='text-center mt-5 p-2'>My Orders</h3>
-            <div className='row row-cols-1 row-cols-md-3 g-4 mb-5 pb-5 ms-2 me-2'>
-                {
-                    userOrders.map((userOrder, i) =>
-                        <div key={i}>
-                            <div className="col">
-                                <div className="card">
-                                    <img src={userOrder.image} style={{height:"451px", objectFit:'cover'}} className="card-img-top" alt="..." />
-                                    <div className="card-body">
+                <h3 className='text-center mt-5 p-2'>My Orders</h3>
+                <div className='row row-cols-1 row-cols-md-3 g-4 mb-5 pb-5 ms-2 me-2'>
+                    {
+                        userOrders.map((userOrder, i) =>
+                            <div key={i}>
+                                <div className="col">
+                                    <div className="card">
+                                        <img src={userOrder.image} style={{ height: "451px", objectFit: 'cover' }} className="card-img-top" alt="..." />
+                                        <div className="card-body">
 
-                                        <h3 className='fw-bold mb-3'>{userOrder.name}</h3>
-                                        <h5 className='fs-5 fw-light'>Travel Date: {userOrder.date}</h5>
-                                        <h5 className='fs-5 fw-light'>Person: {userOrder.person}</h5>
-                                        <h6 className='fw-normal'>${userOrder.price}</h6>
+                                            <h3 className='fw-bold mb-3'>{userOrder.name}</h3>
+                                            <h5 className='fs-5 fw-light'>Travel Date: {userOrder.date}</h5>
+                                            <h5 className='fs-5 fw-light'>Person: {userOrder.person}</h5>
+                                            <h6 className='fw-normal'>${userOrder.price}</h6>
 
-                                        {/* <Link to={`/details/${service.id}`}>
-                                            <button style={{ backgroundColor: '#9bb8b7' }} className='p-2 mt-2 btn'>See More</button>
-                                        </Link> */}
-
+                                            <button onClick={() => handleCancleOrder(userOrder._id)} style={{ backgroundColor: '#9bb8b7' }} className='p-2 mt-2 btn'>Cancle Order</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>)
-                }
+                            </div>)
+                    }
+                </div>
+                <Footer></Footer>
             </div>
-            <Footer></Footer>
-        </div>
         </div>
     );
 };
